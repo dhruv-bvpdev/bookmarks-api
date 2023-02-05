5f9abf25be46acbe601c2f2899dccf29d47a34f6
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2023-02-05 18:03:51 +0530

    feat: :passport_control: setting up signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,36 @@
-import { Injectable } from '@nestjs/common'
+import { ForbiddenException, Injectable } from '@nestjs/common'
+import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
 import { PrismaService } from '@/prisma/prisma.service'
+import { AuthDto } from '@/auth/dto/auth.dto'
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
-    return 'I am the signup endpoint'
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password)
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash
+        }
+      })
+
+      delete user.hash
+
+      return user
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials Taken')
+        }
+      }
+      throw error
+    }
   }
+
   signin() {
     return 'I am the signin endpoint'
   }
