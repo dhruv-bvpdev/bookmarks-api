b627f35cbd0a1fb786a5f2a7a1e384260fd94fa2
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2023-02-05 18:47:31 +0530

    feat: :white_check_mark: completed basic authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,7 +31,20 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return 'I am the signin endpoint'
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email
+      }
+    })
+
+    if (!user) throw new ForbiddenException('Credentials incorrect')
+
+    const pwMatches = await argon.verify(user.hash, dto.password)
+
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect')
+
+    delete user.hash
+    return user
   }
 }
